dee3f470f2c6fa8d0e5b2b8612cca8dc7f06aea3
HuangJiangTao <502636128@qq.com>
2017-10-16 16:03:02 +0800

    1.我增加了一个新的类

---
+++ tyijkyujrtj/tyijkyujrtj/New Group/sdfgfdg.h
@@ -0,0 +1,13 @@
+//
+//  sdfgfdg.h
+//  tyijkyujrtj
+//
+//  Created by 看看智能 on 2017/10/16.
+//  Copyright © 2017年 peter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sdfgfdg : NSObject
+
+@end
---
+++ tyijkyujrtj/tyijkyujrtj/New Group/sdfgfdg.m
@@ -0,0 +1,21 @@
+//
+//  sdfgfdg.m
+//  tyijkyujrtj
+//
+//  Created by 看看智能 on 2017/10/16.
+//  Copyright © 2017年 peter. All rights reserved.
+//
+
+#import "sdfgfdg.h"
+
+@implementation sdfgfdg
+-(instancetype)init
+{
+    if (self == [super init]) {
+        NSLog(@"这里我怎加了一个新的类");
+    }
+    return self;
+    
+    
+}
+@end
